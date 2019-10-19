824bc57e817941cc382153c69444dec95bd55149
enz54 <ericnzenga@gmail.com>
2019-10-19 10:33:15 +0200

    UPDATE modified app.js

--- src/app.js
+++ src/app.js
@@ -27,16 +27,16 @@ app.get('/transfer', (req,res) => res.render('transfer'));
 app.post('/transfer', (req,res) => {
     accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount,10);
-    const accountJSON = JSON.stringify(accounts,null, 4);
-    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountJSON,'utf8');
+    const accountsJSON = JSON.stringify(accounts,null, 4);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
     res.render('transfer', { message:'Transfer Completed'});
 });
-app.get('payment', (req,res)=> res.render('payment', {account:accounts.credit}));
-app.post('payment', (req,res)=> {
+app.get('/payment', (req,res)=> res.render('payment', {account:accounts.credit}));
+app.post('/payment', (req,res)=> {
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount, 10);
-    const accountJSON = JSON.stringify(accoù, null,4);
-    fs.writeFileSync(path.join(__dirname,'json','accounts.json'), accountJSON,'utf8');
+    const accountsJSON = JSON.stringify(accounts, null,4);
+    fs.writeFileSync(path.join(__dirname,'json','accounts.json'), accountsJSON,'utf8');
     res.render('payment', {message:'Payment Successful', account:accounts.credit});
 });
 app.get('/profile', (req, res) => {res.render('profile',{user:users[0]})});
